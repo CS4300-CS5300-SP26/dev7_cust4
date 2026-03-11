d071a40bd55d0b52f0ef2496af7b447ff55d9dc2
Lea <lkarsanb@uccs.edu>
2026-03-11 01:16:11 +0000

    Initial user accounts/

--- cinelog/home/urls.py
+++ cinelog/home/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from .views import index
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', index, name="index"),
+    path('', views.index, name="index"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    # path("signup/", views.signup_html, name="signup"),
 ]
