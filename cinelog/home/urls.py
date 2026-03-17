3ff3af8798db6c451cd314df74b7445482a781b3
Mingma Sherpa <mingmashrp23@gmail.com>
2026-03-17 06:14:39 +0000

    Added routes for movie main page and detail page

--- cinelog/home/urls.py
+++ cinelog/home/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name="landing"),
     path("accounts/", include("django.contrib.auth.urls")),
     # path("signup/", views.signup_html, name="signup"),
+    path("movies/", views.movies_view, name="movies"),
+    path('movies/<int:movie_id>/', views.movie_detail_view, name='movie_detail'),
+
 ]
