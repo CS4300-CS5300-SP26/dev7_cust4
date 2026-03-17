ceb716274f9539999725f3834ecd43c79e8424d7
eboyd3 <eboyd3@uccs.edu>
2026-03-17 20:47:03 +0000

    Added CSRF trusted origins to settings.py for veriication in production environment

--- cinelog/cinelog/settings.py
+++ cinelog/cinelog/settings.py
@@ -123,6 +123,9 @@ DATABASES = {
     )
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://cinelog-service-production.up.railway.app',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
