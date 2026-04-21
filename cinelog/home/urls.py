fb34b9fb570c443146cf01acf6a5b67e2e346387
Bryan Hancock <bhancock@uccs.edu>
2026-04-21 08:50:21 -0600

    feat: add set-rating-mode URL endpoint for AJAX preference saving (#4)

--- cinelog/home/urls.py
+++ cinelog/home/urls.py
@@ -40,4 +40,5 @@ urlpatterns = [
     path('calendar/', views.calendar_view, name='calendar'),
     path('calendar/events/', views.calendar_events_api, name='calendar_events'),
     path('movies/<int:movie_id>/where-to-watch/', views.where_to_watch_view, name='where_to_watch'),
+    path('movies/set-rating-mode/', views.set_rating_mode, name='set_rating_mode'),
 ]
